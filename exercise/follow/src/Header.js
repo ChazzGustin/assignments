288d9204a86605958d0c6bba8b2f3783f44016e6
Chazz Gustin <chazzgustin6@gmail.com>
2018-06-26 14:51:38 -0600

    react business

---
+++ exercise/follow/src/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Header() {
+    return (
+        <h1>title</h1>
+    )
+}
+
+export default Header;
---
+++ exercise/problemSolvingPractice/app.js
@@ -0,0 +1,56 @@
+function large(arr) {
+    var big = arr[0];
+
+    if (arr[0] === undefined) {
+        console.log("The array was empty.");
+    } else {
+        for (i = 1; i < arr.length; i++) {
+            if (arr[i] > big) {
+                big = arr[i];
+            }
+        }
+    }
+    return big;
+}
+
+var testArr = [3, 5, 2, 8, 1];
+// var testArr = [13, 5, 2, 8, 1];
+// var testArr = [13, 5, 2, 8, 100];
+// var testArr = [3];
+// var testArr = [];
+console.log(`\nTesting large`);
+console.log(large(testArr));
+
+
+function lettersWithStrings(arr, char) {
+    var resultArr = [];
+
+    for (var i = 0; i < arr.length; i++) {
+        if (arr[i].indexOf(char) != -1) {
+            resultArr.push((arr[i]));
+        }
+    }
+    return resultArr;
+}
+
+testStrArr = ["#3", "$$$", "C%3!", "Hey!$"];
+char = "$";
+console.log(`\nTesting lettersWithStrings`);
+console.log(`Find ${char} in ${testStrArr}`);
+console.log(lettersWithStrings(testStrArr, char));
+
+
+
+
+
+function isDivisible(num1, num2) {
+
+    if (num1 % num2 === 0) {
+        return true;
+    } else return false;
+}
+
+console.log(`\nTesting isDivisible`);
+console.log(isDivisible(4, 2));
+console.log(isDivisible(9, 3));
+console.log(isDivisible(15, 4));
